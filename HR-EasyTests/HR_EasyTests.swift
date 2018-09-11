636b8b6518bcf878a7551f98952882c3fdf5fc89
Neftali-Samarey <neftali.quintero@cix.csi.cuny.edu>
2018-09-11 12:49:23 -0400

    Initial Commit

---
+++ HR-EasyTests/HR_EasyTests.swift
@@ -0,0 +1,36 @@
+//
+//  HR_EasyTests.swift
+//  HR-EasyTests
+//
+//  Created by Neftali Samarey on 9/11/18.
+//  Copyright © 2018 Neftali Samarey. All rights reserved.
+//
+
+import XCTest
+@testable import HR_Easy
+
+class HR_EasyTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
